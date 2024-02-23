8f840b256918abbbd3901351685bccae3ac83c20
Viacheslav Androsiuk <vyacheslavandrosiuk@gmail.com>
2024-02-24 01:44:44 +0200

    refactor: The Inheritance-based solution to cyclic import problem pattern has been implemented

---
+++ i_game_reloader.py
@@ -0,0 +1,18 @@
+"""
+    This module is created for the inheritance-based solution to a cyclic
+import problem pattern.
+"""
+
+
+class IGameReloader:
+    """
+        The parent class which has been created for the inheritance-based
+    solution to a cyclic import problem pattern.
+    """
+
+    def reload(self) -> None:
+        """
+        The method should be inherited.
+        :return: None
+        """
+        pass
