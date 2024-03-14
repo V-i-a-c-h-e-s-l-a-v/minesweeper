313b2c68c8c3c424f0a1b4afa3ad05fe968c002e
Viacheslav Androsiuk <vyacheslavandrosiuk@gmail.com>
2024-03-14 23:50:33 +0200

    The timer pause/start feature has been added to the Settings menu bar.
    Now, when the Settings bar is opened, the timer switches to the 'Stop' mode. When the Settings bar is closed, the time countdown continues.

---
+++ show_all_cell.py
@@ -0,0 +1,52 @@
+from gui import MyButton
+import tkinter as tk
+import config
+
+
+class AllCellShow:
+    """
+    Opening the closed minefield cells when the end game event has occurred.
+        Methods:
+            - show_all_cell: Opening the closed minefield cells when the end
+            game event has occurred.
+
+    """
+
+    @staticmethod
+    def show_all_cell(buttons: list[list[MyButton]]) -> None:
+        """
+        Opening the closed minefield cells when the end game event
+        has occurred.
+        :param buttons: The List of lists represents a grid of Tkinter
+        buttons.
+        :return: None.
+        """
+        for i in range(1, config.ROW + 1):
+            for j in range(1, config.COLUMN + 1):
+                btn = buttons[i][j]
+                if btn.is_mine and not btn.is_open:
+                    btn.config(
+                        text="🚩",
+                        state="disabled",
+                    )
+
+                elif (
+                    not btn.is_mine
+                    and not btn.is_open
+                    and btn.adjacent_mines_count != 0
+                ):
+                    btn.config(
+                        text=f"{btn.adjacent_mines_count}",
+                        state="disabled",
+                        relief=tk.SUNKEN,
+                    )
+                elif (
+                    not btn.is_mine
+                    and not btn.is_open
+                    and btn.adjacent_mines_count == 0
+                ):
+                    btn.config(
+                        text="",
+                        state="disabled",
+                        relief=tk.SUNKEN,
+                    )
